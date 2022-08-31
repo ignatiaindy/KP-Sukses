e0e2fab12d26d50e90bc886abcee24b6fbfbacdf
ignatiaindy <ignatiaindyy@gmail.com>
2022-08-31 09:33:32 +0700

    tes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- test -->
-
+    //indy
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/boxicons.min.css">
         <link rel="stylesheet" href="assets/css/flaticon.css">
